d9fd8c98abd2cc6f85d26653b6d0889b195ec7bf
carzy <carzy.dong@tonmx.com>
2020-08-26 16:22:18 +0800

    init

---
+++ src/main/java/com/dxp/cloud/DCloudApplication.java
@@ -0,0 +1,16 @@
+package com.dxp.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author carzy
+ */
+@SpringBootApplication
+public class DCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DCloudApplication.class, args);
+    }
+
+}
