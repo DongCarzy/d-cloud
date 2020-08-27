f0ee3693d61239bcbe57130e67bf14e38df3eaf1
carzy <carzy.dong@tonmx.com>
2020-08-27 17:31:56 +0800

    fix: README

---
+++ d-gateway/src/main/java/com/dxp/cloud/gateway/Application.java
@@ -0,0 +1,11 @@
+package com.dxp.cloud.gateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author carzy
+ * @date 2020/8/26
+ */
+@SpringBootApplication
+public class Application {
+}
